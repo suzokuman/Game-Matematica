906b4a0fed7f671bbf1dd0cb8c68c5ed9884e7b1
suzokuman <suzoku@gmail.com>
2025-07-12 19:44:38 -0300

    Update PlayerForm.tsx

--- src/components/PlayerForm.tsx
+++ src/components/PlayerForm.tsx
@@ -6,6 +6,7 @@ import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
 
 const playerSchema = z.object({
   name: z.string().min(1, "Nome é obrigatório"),
@@ -51,7 +52,22 @@ const PlayerForm: React.FC<PlayerFormProps> = ({ onSubmitPlayerInfo }) => {
             <FormItem>
               <FormLabel>Série</FormLabel>
               <FormControl>
-                <Input placeholder="Digite sua série" {...field} />
+                <Select value={field.value} onValueChange={field.onChange} defaultValue="">
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Selecione a série" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="1º ano">1º ano</SelectItem>
+                    <SelectItem value="2º ano">2º ano</SelectItem>
+                    <SelectItem value="3º ano">3º ano</SelectItem>
+                    <SelectItem value="4º ano">4º ano</SelectItem>
+                    <SelectItem value="5º ano">5º ano</SelectItem>
+                    <SelectItem value="6º ano">6º ano</SelectItem>
+                    <SelectItem value="7º ano">7º ano</SelectItem>
+                    <SelectItem value="8º ano">8º ano</SelectItem>
+                    <SelectItem value="9º ano">9º ano</SelectItem>
+                  </SelectContent>
+                </Select>
               </FormControl>
               <FormMessage />
             </FormItem>
